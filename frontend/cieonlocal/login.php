c975fed75285edfc74d9eb1d60fe785fdb462081
brighthermit <49891127+brighthermit@users.noreply.github.com>
2021-06-07 16:45:49 +0700

    Update
    login sentences

--- frontend/cieonlocal/login.php
+++ frontend/cieonlocal/login.php
@@ -281,7 +281,7 @@ error_reporting(0);
 							// echo $sql;
 						}
 					} else {
-						echo $cls_conn->show_message('Username or Password is incorrect If you forget your password please use forget password buttom');
+						echo $cls_conn->show_message('Username or password is incorrect. If you forgot your password, please use the ‘forget password’ button.');
 						// echo $sql;
 						// echo $sql2;
 					}
